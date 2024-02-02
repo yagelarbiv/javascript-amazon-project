28e028ff722d9ac12e2137238242cd5ab528cb78
yagelarbiv <142971084+yagelarbiv@users.noreply.github.com>
2024-02-02 23:42:06 +0200

    using imports

--- Scripts/Amazon.js
+++ Scripts/Amazon.js
@@ -1,3 +1,6 @@
+import {cart} from '../data/cart.js';
+import {products} from '../data/products.js';
+
 let component = '';
 products.forEach(product => {
   component += ` 
@@ -40,7 +43,7 @@ products.forEach(product => {
 
     <div class="product-spacer"></div>
 
-    <div class="added-to-cart-${product.id}">
+    <div class="added-to-cart">
       <img src="../images/icons/checkmark.png">
       Added
     </div>
